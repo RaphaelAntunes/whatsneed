d185c42ddb9f6733d4ba95568de7a34705ad1fa7
Raphael Antunes <faelpinheiroantunes@gmail.com>
2024-06-15 12:11:20 -0300

    Servidor cors

--- frontend/sv.js
+++ frontend/sv.js
@@ -1,13 +1,36 @@
 // server.js
 const cors_proxy = require('cors-anywhere');
+const https = require('https');
+const fs = require('fs');
 
-const host = 'localhost';
+const host = 'crm.whatsneed.com.br'; // Alterado para crm.whatsneed.com.br
 const port = 9999;
 
-cors_proxy.createServer({
+// Configurações para o CORS Anywhere
+const corsOptions = {
   originWhitelist: [], // Permitir todas as origens
   requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
+};
+
+// Configurações para o servidor HTTPS
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/crm.whatsneed.com.br/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/crm.whatsneed.com.br/fullchain.pem')
+};
+
+// Criação do servidor CORS Anywhere
+const corsServer = cors_proxy.createServer(corsOptions);
+
+// Criação do servidor HTTPS
+const server = https.createServer(httpsOptions);
+
+// Adiciona o CORS Anywhere como middleware no servidor HTTPS
+server.addListener('request', (req, res) => {
+  corsServer.emit('request', req, res);
+});
+
+// Inicia o servidor HTTPS
+server.listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
